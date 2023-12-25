c3d79261291857d4214300d9d53c46ee3e943e45
Narthana Epa <narthana.epa+compressbronze@gmail.com>
2023-12-26 02:43:17 +0530

    Make User.ExternalId a uuid

--- internal/models/user.go
+++ internal/models/user.go
@@ -4,28 +4,30 @@ import (
 	"fmt"
 	"reflect"
 	"strings"
+
+	"github.com/google/uuid"
 )
 
 type User struct {
-	Id             int      `json:"_id"`
-	URL            string   `json:"url"`
-	ExternalId     string   `json:"external_id"`
-	Name           string   `json:"name"`
-	Alias          string   `json:"alias"`
-	CreatedAt      string   `json:"created_at"`
-	Active         bool     `json:"active"`
-	Verified       bool     `json:"verified"`
-	Shared         bool     `json:"shared"`
-	Locale         string   `json:"locale"`
-	Timezone       string   `json:"timezone"`
-	LastLoginAt    string   `json:"last_login_at"`
-	Email          string   `json:"email"`
-	Phone          string   `json:"phone"`
-	Signature      string   `json:"signature"`
-	OrganizationId int      `json:"organization_id"`
-	Tags           []string `json:"tags"`
-	Suspended      bool     `json:"suspended"`
-	Role           string   `json:"role"`
+	Id             int       `json:"_id"`
+	URL            string    `json:"url"`
+	ExternalId     uuid.UUID `json:"external_id"`
+	Name           string    `json:"name"`
+	Alias          string    `json:"alias"`
+	CreatedAt      string    `json:"created_at"`
+	Active         bool      `json:"active"`
+	Verified       bool      `json:"verified"`
+	Shared         bool      `json:"shared"`
+	Locale         string    `json:"locale"`
+	Timezone       string    `json:"timezone"`
+	LastLoginAt    string    `json:"last_login_at"`
+	Email          string    `json:"email"`
+	Phone          string    `json:"phone"`
+	Signature      string    `json:"signature"`
+	OrganizationId int       `json:"organization_id"`
+	Tags           []string  `json:"tags"`
+	Suspended      bool      `json:"suspended"`
+	Role           string    `json:"role"`
 }
 
 func (u *User) Fields() map[string]bool {
